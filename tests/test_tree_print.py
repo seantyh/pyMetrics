785b996bbf1028724e4c73a86b30fc7762042e2c
SeanTseng <seantyh@gmail.com>
2018-09-24 21:25:16 +0800

    add test_tree_deep test codes

--- tests/test_tree_print.py
+++ tests/test_tree_print.py
@@ -18,6 +18,11 @@ def test_tree_print_func():
     print("depth: " + str(pyMetrics.tree_depth(tree)))
 
 def test_depth_simple():
-    tree = ast.parse("a=a+1")
-    pyMetrics.print_tree(tree)
+    tree = ast.parse("a=a+1;b=b+1;c=c+2;d=d+3")    
+    print("nnode: " + str(pyMetrics.node_count(tree)))
+    print("depth: " + str(pyMetrics.tree_depth(tree)))
+
+def test_tree_deep():
+    tree = ast.parse("[y for y in range(x) for x in range(5) if x % 2]")    
+    print("nnode: " + str(pyMetrics.node_count(tree)))
     print("depth: " + str(pyMetrics.tree_depth(tree)))
